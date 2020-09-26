c0f5282b76b03292266fa69edec940f6722d1c67
hushell <dom343@gmail.com>
2020-09-26 00:10:46 +0000

    update

--- config.sh
+++ config.sh
@@ -1,8 +1,12 @@
+apt-get update && apt-get install -y \
+	vim screen htop wget gcc
+
 # Python packages
 pip install torchaudio==0.5.0 soundfile tqdm jupyter matplotlib 
+pip install numpy Cython
 
 # bashrc
-~/miniconda3/bin/conda init
+~/miniconda/bin/conda init
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 mv git-completion.bash ~/.git-completion.bash
 cp gitconfig ~/.gitconfig
