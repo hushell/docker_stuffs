b0e65e1ce803b4f3e5a3418b478f71626c995609
hushell <dom343@gmail.com>
2020-10-23 22:58:30 +0000

    fix git completion bug

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 sudo apt-get update && sudo apt-get install -y \
 	vim screen htop wget gcc libsndfile1
+sudo apt-get install -y git-core bash-completion
 
 # Python packages
 pip install torchaudio==0.5.0 soundfile tqdm jupyter jupyterlab matplotlib 
@@ -8,15 +9,11 @@ pip install -r requirements.txt
 
 # bashrc
 ~/miniconda/bin/conda init
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-mv git-completion.bash ~/.git-completion.bash
-cp gitconfig ~/.gitconfig
-echo "source ~/.git-completion.bash" >> ~/.bashrc
 echo "alias screen='screen -U'" >> ~/.bashrc
 echo "defshell -bash" > ~/.screenrc
-
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
 source ~/.bashrc
-
+cp gitconfig ~/.gitconfig
 
 # asteroid
 cd /app/hushell/UploadAI/asteroid-avatar
